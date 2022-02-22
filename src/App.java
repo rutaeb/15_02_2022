88c4636c0fafb0f248b0bc01cd88d2014330268e
Your Name~ <you@example.com>
2022-02-22 13:30:57 +0200

    ok

--- src/App.java
+++ src/App.java
@@ -2,5 +2,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("MIKI IS THE BEST");
+        System.out.println("MIKI IS THE BEST");
+        System.out.println("MIKI IS THE BEST");
+        System.out.println("MIKI IS THE BEST");
+        
     }
 }
